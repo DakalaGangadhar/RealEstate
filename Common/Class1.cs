221ea4d6b75b4f297490d41c3ac7d76adf19bdd3
DakalaGangadhara <113017865+DakalaGangadhar@users.noreply.github.com>
2025-03-01 17:57:00 +0530

    Class1.cs

--- Common/Class1.cs
+++ Common/Class1.cs
@@ -5,5 +5,9 @@
         public string str { get; set; }
 
         public string strTest { get; set; }
+
+        public string Name { get; set; }
+
+        public string Address { get; set; }
     }
 }
